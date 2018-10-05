667e9f5cb5ba85372e113bcfbd3822c16cac8d92
ItsDubC <ItsDubC@gmail.com>
2018-10-04 21:06:25 -0500

    Refactored Account view model classes into their own cs files un the ViewModels folder

---
+++ Vidly/ViewModels/ExternalLoginConfirmationViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Vidly.ViewModels
+{
+    public class ExternalLoginConfirmationViewModel
+    {
+        [Required]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required]
+        [Display(Name = "Drivers License")]
+        public string DriversLicense { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        [Display(Name = "Phone")]
+        [Phone]
+        public string PhoneNumber { get; set; }
+    }
+}
