f1de4451b5c32ff4e58c88a7be4d97ea204baf46
itsdubc <itsdubc@gmail.com>
2018-10-06 08:25:54 -0500

    Implemented CreateNewRentals method.

--- Vidly/Controllers/Api/RentalsController.cs
+++ Vidly/Controllers/Api/RentalsController.cs
@@ -5,14 +5,51 @@ using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 using Vidly.Dtos;
+using Vidly.Models;
 
 namespace Vidly.Controllers.Api
 {
     public class NewRentalsController : ApiController
     {
+        private ApplicationDbContext _context;
+
+        public NewRentalsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
         [HttpPost]
         public IHttpActionResult CreateNewRentals(NewRentalDto newRental)
         {
+            if (!ModelState.IsValid)
+                return BadRequest();
+
+            var customer = _context.Customers.SingleOrDefault(x => x.Id == newRental.CustomerId);
+
+            if (customer == null)
+                return NotFound();
+
+            var movies = _context.Movies.Where(x => newRental.MovieIds.Contains(x.Id)).ToList();
+
+            foreach (var movie in movies)
+            {
+                if (movie.NumberInStock == 0)
+                    return BadRequest("Movie is not available.");
+
+                movie.NumberInStock--;
+
+                var rental = new Rental()
+                {
+                    DateRented = DateTime.Now,
+                    Movie = movie,
+                    Customer = customer
+                };
+
+                _context.Rentals.Add(rental);
+            }
+
+            _context.SaveChanges();
+
             return Ok();
         }
     }
